abbaadd76cea5b8c48afdab553bdec68f70f6fcd
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-04-17 20:36:52 -0500

    ajuste

--- src/app/modules/coches/coches-routing.module.ts
+++ src/app/modules/coches/coches-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaMarcasComponent } from './lista-marcas/lista-marcas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaMarcasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/coches/coches.module.ts
+++ src/app/modules/coches/coches.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CochesRoutingModule } from './coches-routing.module';
-
+import { ListaMarcasComponent } from './lista-marcas/lista-marcas.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListaMarcasComponent
+  ],
   imports: [
     CommonModule,
     CochesRoutingModule
