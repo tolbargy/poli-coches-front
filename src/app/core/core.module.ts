5aa4ca5e5ced7062631692d6c9b4a3ff1a188b49
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-04-19 20:40:13 -0500

    dependecy angular/cdk

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SuiSidebarModule } from 'ngx-semantic/modules/sidebar/'
 
 @NgModule({
   declarations: [
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SuiSidebarModule
+  ],
+  exports: [
+    SidebarComponent
   ]
 })
 export class CoreModule { }
