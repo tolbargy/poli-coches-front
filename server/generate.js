ae80f6e63abf53e78ae8a718caa32d25f6290465
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-04-20 00:28:41 -0500

    ajuste database

--- server/generate.js
+++ server/generate.js
@@ -1,13 +1,27 @@
 var faker = require('faker');
 
-var database = { marcas: [] };
+var database = {
+  marcas: [],
+  modelos: []
+};
 
-for (var i = 1; i <= 50; i++) {
+for (var i = 1; i <= 16; i++) {
   database.marcas.push({
     id: i,
     nombre: faker.company.companyName(),
     urlLogo: faker.image.city()
   });
+
+  for (var otroI = 1; otroI <= 5; otroI++) {
+    database.modelos.push({
+      id: otroI,
+      nombre: faker.vehicle.model(),
+      version: faker.address.zipCode(),
+      combustible: faker.vehicle.type(),
+      potenciaHp: faker.datatype.number(),
+      marcaId: i
+    });
+  }
 }
 
 console.log(JSON.stringify(database));
