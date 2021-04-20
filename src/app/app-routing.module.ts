6b90a92f48ac9802582475634c0b5869030b3fcb
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-04-19 23:09:57 -0500

    avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from '@core/inicio/inicio.component';
 
 const routes: Routes = [
     {
-        path: '',
+        path: "",
+        redirectTo: "inicio",
+        pathMatch: "full"
+    },
+    {
+        path: 'inicio',
+        component: InicioComponent
+    },
+    {
+        path: 'coches',
         loadChildren: () => import('@modules/coches/coches.module').then(m => m.CochesModule)
     }
 ];
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    InicioComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidebarComponent
