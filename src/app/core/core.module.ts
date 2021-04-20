d2766eb271643f926bb561c8b864075b8e3412d0
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-04-19 22:32:53 -0500

    ajustando y eliminando ngx-semantic

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { SuiSidebarModule } from 'ngx-semantic/modules/sidebar/'
 
 @NgModule({
   declarations: [
     SidebarComponent
   ],
   imports: [
-    CommonModule,
-    SuiSidebarModule
+    CommonModule
   ],
   exports: [
     SidebarComponent
